f7d2d325bb2bfa8f794a6f6c4183d4a5de6f9201
nghia <naovu@protonmail.com>
2023-08-19 18:33:30 +0700

    CONFIG: cloudinary

---
+++ services/cloudinary.js
@@ -0,0 +1,9 @@
+import { v2 as cloudinary } from 'cloudinary';
+
+cloudinary.config({
+	cloud_name: 'dggxjymsy',
+	api_key: '364697723745282',
+	api_secret: 'cD4Z75QJjcWllnFGwO8zWmycFZ4',
+});
+
+export default cloudinary;
