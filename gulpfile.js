1e6f01477f2b567c0f406e22b5aabe9015f462c9
nghia <naovu@protonmail.com>
2023-08-28 01:01:34 +0700

    fix: :fire: fixed bug ReferenceError: exports is not defined in ES module scope

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,9 @@ import uglify from 'gulp-uglify';
 import rename from 'gulp-rename';
 
 import { pipeline } from 'readable-stream';
+import ts from 'gulp-typescript';
+
+const tsProject = ts.createProject('./tsconfig.json');
 
 gulp.task('compress', function () {
 	return pipeline(
@@ -10,5 +13,16 @@ gulp.task('compress', function () {
 		uglify(),
 		rename({ extname: '.min.js' }),
 		gulp.dest('build'),
+	);
+});
+
+gulp.task('default', function () {
+	return (
+		tsProject
+			.src()
+			.pipe(tsProject())
+			.js.pipe(uglify())
+			// .pipe(rename({ suffix: '.min' }))
+			.pipe(gulp.dest('build'))
 	);
 });
