dd481fc736d2aaedc92eb55bf21b82c3b7d9970e
nghia <naovu@protonmail.com>
2023-08-27 23:56:04 +0700

    feat(index file): :art: add rename function in gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 import gulp from 'gulp';
 import uglify from 'gulp-uglify';
+import rename from 'gulp-rename';
+
 import { pipeline } from 'readable-stream';
 
 gulp.task('compress', function () {
-	return pipeline(gulp.src('build/*.js'), uglify(), gulp.dest('build'));
+	return pipeline(
+		gulp.src('build/*.js'),
+		uglify(),
+		rename({ extname: '.min.js' }),
+		gulp.dest('build'),
+	);
 });
