38412f79470873df0eb6214f4bd13b39b12b2661
nghia <naovu@protonmail.com>
2023-08-28 16:56:51 +0700

    fix:

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,20 @@
 import gulp from 'gulp';
 import uglify from 'gulp-uglify';
 import rename from 'gulp-rename';
-
-import { pipeline } from 'readable-stream';
 import ts from 'gulp-typescript';
 
 const tsProject = ts.createProject('./tsconfig.json');
 
 gulp.task('compress', function () {
-	return pipeline(
-		gulp.src('build/*.js'),
-		uglify(),
-		rename({ extname: '.min.js' }),
-		gulp.dest('build'),
-	);
+	return gulp
+		.src('build/index.js')
+		.pipe(uglify())
+		.pipe(rename('index.min.js'))
+		.pipe(gulp.dest('build'));
 });
 
-gulp.task('default', function () {
-	return (
-		tsProject
-			.src()
-			.pipe(tsProject())
-			.js.pipe(uglify())
-			// .pipe(rename({ suffix: '.min' }))
-			.pipe(gulp.dest('build'))
-	);
+gulp.task('compiler', function () {
+	return tsProject.src().pipe(tsProject()).pipe(gulp.dest('build'));
 });
+
+gulp.task('default', gulp.series('compiler', 'compress'));
