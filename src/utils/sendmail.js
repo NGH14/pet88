e892ca714bc5dd7f6edab4a82c99cb352b60fd56
nghia <naovu@protonmail.com>
2023-08-30 00:54:56 +0700

    style: :art: rename send mail func

---
+++ src/utils/sendmail.js
@@ -0,0 +1,21 @@
+import nodemailer from 'nodemailer';
+
+async function sendMail(dataSend, template) {
+	let transporter = nodemailer.createTransport({
+		host: 'smtp.gmail.com',
+		port: 587,
+		secure: false,
+		auth: JSON.parse(process.env.MAIL_ACCOUNT),
+	});
+
+	// send mail with defined transport object
+	await transporter.sendMail({
+		from: '"Pet88 Service" <servicepet88@gmail.com>', // sender address
+		to: dataSend.recipient, // list of receivers
+		subject: dataSend.subject, // Subject line
+		text: dataSend.text || '', // plain text body
+		html: template, // html body
+	});
+}
+
+export default sendMail;
