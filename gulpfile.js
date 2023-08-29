47795d4c425ad81327986fa1b590b26d1b27502c
nghia <naovu@protonmail.com>
2023-08-30 01:16:55 +0700

    feat(index file): :fire: config gulp to change setup doppler evironment

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,64 @@
-import gulp from 'gulp';
-import uglify from 'gulp-uglify';
-import rename from 'gulp-rename';
-import ts from 'gulp-typescript';
+const gulp = require('gulp');
+const uglify = require('gulp-uglify');
+const rename = require('gulp-rename');
+const ts = require('gulp-typescript');
+const replace = require('gulp-replace');
+const exec = require('gulp-exec');
+const log = require('fancy-log');
+const util = require('util');
 
-const tsProject = ts.createProject('./tsconfig.json');
+util.inspect.stye = '';
 
-gulp.task('compress', function () {
+gulp.on('error', (errors) => log.error(errors).process.exit.bind(process, 1));
+
+gulp.task('setup-doppler', function () {
+	const options = {
+		continueOnError: false, // default = false, true means don't emit error event
+		pipeStdout: false, // default = false, true means stdout is written to file.contents
+	};
+	const reportOptions = {
+		err: true, // default = true, false means don't write err
+		stderr: true, // default = true, false means don't write stderr
+		stdout: true, // default = true, false means don't write stdout
+	};
 	return gulp
-		.src('build/index.js')
-		.pipe(uglify())
-		.pipe(rename('index.min.js'))
-		.pipe(gulp.dest('build'));
+		.src('./')
+		.pipe(exec(() => `doppler setup --no-interactive`, options))
+		.on('end', () => log.info('✅ Setup Doppler Success'))
+		.pipe(exec.reporter(reportOptions));
 });
 
-gulp.task('compiler', function () {
-	return tsProject.src().pipe(tsProject()).pipe(gulp.dest('build'));
+gulp.task('server', function () {
+	const options = {
+		continueOnError: false, // default = false, true means don't emit error event
+		pipeStdout: true, // default = false, true means stdout is written to file.contents
+	};
+	const reportOptions = {
+		err: true, // default = true, false means don't write err
+		stderr: false, // default = true, false means don't write stderr
+		stdout: false, // default = true, false means don't write stdout
+	};
+	return gulp
+		.src('./')
+		.on('end', () => log.info('⚙️ Start server'))
+		.pipe(exec(() => `npm start`, options))
+		.pipe(exec.reporter(reportOptions));
 });
 
-gulp.task('default', gulp.series('compiler', 'compress'));
+gulp.task('prd-doppler', () => {
+	return gulp
+		.src('./doppler.yaml')
+		.pipe(replace('be-dev', 'prd'))
+		.on('end', () => log.info('✅ Changed Doppler Config'))
+		.pipe(gulp.dest('./'));
+});
+
+gulp.task('dev-doppler', () => {
+	return gulp
+		.src('./doppler.yaml')
+		.pipe(replace('prd', 'be-dev'))
+		.pipe(gulp.dest('./'));
+});
+
+gulp.task('start:prd', gulp.series('prd-doppler', 'setup-doppler', 'server'));
+gulp.task('dev', gulp.series('prd-doppler', 'setup-doppler', 'server'));
