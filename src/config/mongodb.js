4fef6e541e5f52bbb2e0c8419df96d72a8976b1e
nghia <naovu@protonmail.com>
2023-09-01 18:14:31 +0700

    feat(REFACTOR): :art: refactor connect database

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
 	try {
 		mongoose.connect(process.env.MONGO, { dbName: process.env.DB });
+
 		console.log('✅ MongoDB Connected');
 	} catch (error) {
 		console.log(error);
