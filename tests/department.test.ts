d81cd4f8e0cb203616700bcb0512791878d6a9bb
nghia <naovu@protonmail.com>
2023-09-07 00:26:30 +0700

    refactor(REFACTOR): :zap: refactor test to optimize running time

--- tests/department.test.ts
+++ tests/department.test.ts
@@ -1,37 +1,39 @@
 import chai from 'chai';
 
 import mongoose from 'mongoose';
-import { Department, createNewDepartment } from '../src/models/department.ts';
+import {
+	Department,
+	createListDepartments,
+	createNewDepartment,
+} from '../src/models/department.ts';
 import chaiHttp from 'chai-http';
 import { app } from '../src/index.ts';
 import { connectDB } from '../src/config/mongodb.js';
+import { randCity, randStreetAddress } from '@ngneat/falso';
 
 const should = chai.should();
 const expect = chai.expect;
 
 chai.use(chaiHttp);
-describe('Department', () => {
+describe('Department API', () => {
 	const sample = {
 		name: 'test new api',
-		type: 'owner',
-		city: 'Ha Noi',
-		address: 'somewhere',
+		type: 'owner',	
+		city: randCity(),
+		address: randStreetAddress(),
 		title: 'Best Hotel in the City',
 		desc: 'hotel description',
 		services: ['grooming', 'hotel'],
 	};
 	const endPoint = '/api/departments/';
-
-	// Needed to clean the database before each test
 	beforeEach((done) => {
 		connectDB();
-		Department.deleteMany({}, (err) => {
-			done();
-		});
+		Department.deleteMany({}, (err) => {});
+		done();
 	});
 
 	describe('/GET departments', () => {
-		it('should GET the empty array', () => {
+		it('should GET the empty array', (done) => {
 			chai
 				.request(app)
 				.get(endPoint)
@@ -40,13 +42,14 @@ describe('Department', () => {
 					res.body.should.be.a('array');
 					res.body.length.should.be.equal(0);
 				});
+			done();
 		});
-		it('should GET a department by the given id', async () => {
-			const newDepartment = await createNewDepartment(sample);
-			chai
+		it('should GET a department by the given id',  (done) => {
+			let newDepartment = new Department(sample)
+			newDepartment.save((_, department) => {chai
 				.request(app)
-				.get(endPoint + newDepartment._id)
-				.end((err, res) => {
+				.get(endPoint + department._id)
+				.end((_err, res) => {
 					res.should.have.status(200);
 					res.body.should.be.a('object');
 					res.body.should.have.property('_id');
@@ -54,12 +57,13 @@ describe('Department', () => {
 					res.body.should.have.property('services');
 					res.body.should.have.property('city');
 					res.body.should.have.property('title');
-					res.body.should.have.property('_id').eql(newDepartment.id);
-				});
+					res.body.should.have.property('_id').eql(department.id);
+			})});
+			done()
 		});
 	});
 	describe('/POST department', () => {
-		it('should POST department ', () => {
+		it('should POST department ', (done) => {
 			chai
 				.request(app)
 				.post(endPoint)
@@ -74,9 +78,10 @@ describe('Department', () => {
 					res.body.should.have.property('city');
 					res.body.should.have.property('title');
 				});
+			done();
 		});
 
-		it('should not POST department city field', () => {
+		it('should not POST department city field', (done) => {
 			const { city, ...sampleWithoutCityField } = sample;
 			chai
 				.request(app)
@@ -89,21 +94,72 @@ describe('Department', () => {
 					res.body.should.have.property('errors');
 					res.body.errors.should.have.property('city');
 				});
+			done();
+		});
+
+		it('should POST departments', (done) => {
+			chai
+				.request(app)
+				.post(endPoint + 'list')
+				.set('content-type', 'application/json')
+				.send(JSON.stringify([sample, sample]))
+				.end((_err, res) => {
+					res.should.have.status(200);
+					res.body.should.be.a('array');
+					res.body.length.should.be.eql(2);
+				});
+				done()
 		});
 	});
 
 	describe('/PUT/:id department', () => {
-		it('should UPDATE a department given the id', async () => {
-			const newDepartment = await createNewDepartment(sample);
+		it('should UPDATE a department given the id', (done) => {
+			let newDepartment = new Department(sample);
+				chai
+					.request(app)
+					.put(endPoint + newDepartment._id)
+					.send({ city: 'Da Nang' })
+					.end((_err, res) => {
+						res.should.have.status(200);
+						res.body.should.be.a('object');
+						res.body.should.have.property('city').eql('Da Nang');
+					});
+			done();
+		});
+	});
+
+	describe('/DELETE/:id department', () => {
+		it('should DELETE a department given the id', () => {
+			let newDepartment = new Department(sample);
+			newDepartment.save((_, department) => {
+				chai
+					.request(app)
+					.del(endPoint + department._id)
+					.end((_err, res) => {
+						res.should.have.status(200);
+					});
+			});
+		});
+	});
+	describe('/PATH department', () => {
+		it('should DELETE a department given the id', (done) => {
+			let newDepartment1 = new Department(sample);
+			let newDepartment2 = new Department(sample);
+
+			const listID = Array(newDepartment1, newDepartment2).map(
+				(department) => department._id,
+			);
 			chai
 				.request(app)
-				.put(endPoint + newDepartment._id)
-				.send({ city: 'Da Nang' })
+				.patch(endPoint + 'multiple-delete')
+				.set('content-type', 'application/json')
+				.send(JSON.stringify(listID))
 				.end((_err, res) => {
 					res.should.have.status(200);
-					res.body.should.be.a('object');
-					res.body.book.should.have.property('city').eql('Da Nang');
+					res.body.should.be.contain("deleted")
 				});
+			done();
+
 		});
 	});
 });
