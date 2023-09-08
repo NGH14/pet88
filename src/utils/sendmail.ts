097aa9b7d43e19cfc00dcf102ed86edc41e0deb8
nghia <naovu@protonmail.com>
2023-09-08 07:15:57 +0700

    refactor(index file): :zap: refactor send mail utils

--- src/utils/sendmail.ts
+++ src/utils/sendmail.ts
@@ -1,7 +1,9 @@
 import nodemailer from 'nodemailer';
 
-
-async function sendMail(dataSend: nodemailer.SendMailOptions, template: string) {
+async function sendMail(
+	dataSend: nodemailer.SendMailOptions,
+	template: string,
+) {
 	let transport = nodemailer.createTransport({
 		host: 'smtp.gmail.com',
 		port: 587,
@@ -10,14 +12,14 @@ async function sendMail(dataSend: nodemailer.SendMailOptions, template: string) 
 	});
 
 	// send mail with defined transport object
-	transport.sendMail({
+	 const info = await transport.sendMail({
 		from: '"Pet88 Service" <servicepet88@gmail.com>', // sender address
 		to: dataSend.to, // list of receivers
 		subject: dataSend.subject, // Subject line
 		text: dataSend.text || '', // plain text body
 		html: template, // html body
 	});
+	return info
 }
 
 export default sendMail;
-	
