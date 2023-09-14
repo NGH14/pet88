f53472a5f7eead1736bab9c731f7765c449e5121
nghia <naovu@protonmail.com>
2023-09-14 22:01:35 +0700

    style(index file): :art: rename department model

--- src/routes/rooms.js
+++ src/routes/rooms.js
@@ -1,120 +1,120 @@
 import express from 'express';
 
 const router = express.Router();
-import { Hotel } from '../models/hotel.js';
+import { Department } from '../models/department.ts';
 import { Room } from '../models/room.js';
 
 router.post('/:id', async (req, res) => {
-    const hotelId = req.params.id;
-    const newRoom = new Room(req.body);
+	const hotelId = req.params.id;
+	const newRoom = new Room(req.body);
 
-    try {
-        const savedRoom = await newRoom.save();
-        try {
-            await Hotel.findByIdAndUpdate(hotelId, {
-                $push: { rooms: savedRoom._id },
-            });
-        } catch (err) {
-            res.status(500).json(err);
-        }
-        res.status(200).json(savedRoom);
-    } catch (err) {
-        res.json(err);
-    }
+	try {
+		const savedRoom = await newRoom.save();
+		try {
+			await Department.findByIdAndUpdate(hotelId, {
+				$push: { rooms: savedRoom._id },
+			});
+		} catch (err) {
+			res.status(500).json(err);
+		}
+		res.status(200).json(savedRoom);
+	} catch (err) {
+		res.json(err);
+	}
 });
 
 router.put('/:id', async (req, res) => {
-    try {
-        const updatedRoom = await Room.findByIdAndUpdate(
-            req.params.id,
-            {
-                $set: req.body,
-            },
-            { new: true },
-        );
-        res.status(200).json(updatedRoom);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+	try {
+		const updatedRoom = await Room.findByIdAndUpdate(
+			req.params.id,
+			{
+				$set: req.body,
+			},
+			{ new: true },
+		);
+		res.status(200).json(updatedRoom);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.delete('/:id', async (req, res) => {
-    try {
-        await Room.findByIdAndDelete(req.params.id);
-        res.status(200).json('Room has been deleted.');
-    } catch (err) {
-        res.status(500).json(err);
-    }
+	try {
+		await Room.findByIdAndDelete(req.params.id);
+		res.status(200).json('Room has been deleted.');
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.get('/:id', async (req, res) => {
-    try {
-        const room = await Room.findById(req.params.id);
-        res.status(200).json(room);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+	try {
+		const room = await Room.findById(req.params.id);
+		res.status(200).json(room);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.get('/availability/found', async (req, res) => {
-    try {
-        const dates = req.body.dates;
-        const roomList = await Room.find({
-            _id: req.body.id,
-        });
-        const list = roomList.map((element) => {
-            return {
-                ...element.toJSON(),
-                roomNumbers: element.roomNumbers.filter(
-                    (rn) =>
-                        !rn.unavailableDates.some((ud) => !dates.includes(ud)),
-                ),
-            };
-        });
-        res.json(list);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+	try {
+		const dates = req.body.dates;
+		const roomList = await Room.find({
+			_id: req.body.id,
+		});
+		const list = roomList.map((element) => {
+			return {
+				...element.toJSON(),
+				roomNumbers: element.roomNumbers.filter(
+					(rn) =>
+						!rn.unavailableDates.some((ud) => !dates.includes(ud)),
+				),
+			};
+		});
+		res.json(list);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.get('/', async (req, res) => {
-    try {
-        const rooms = await Room.find();
+	try {
+		const rooms = await Room.find();
 
-        res.status(200).json(rooms);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+		res.status(200).json(rooms);
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.put('/availability/:id', async (req, res) => {
-    try {
-        await Room.updateOne(
-            { 'roomNumbers._id': req.params.id },
-            {
-                $push: {
-                    'roomNumbers.$.unavailableDates': req.body.dates,
-                },
-            },
-        );
-        res.status(200).json('Room status has been updated.');
-    } catch (err) {
-        res.status(500).json(err);
-    }
+	try {
+		await Room.updateOne(
+			{ 'roomNumbers._id': req.params.id },
+			{
+				$push: {
+					'roomNumbers.$.unavailableDates': req.body.dates,
+				},
+			},
+		);
+		res.status(200).json('Room status has been updated.');
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 router.patch('/multiple-delete', async (req, res) => {
-    try {
-        await Room.deleteMany({
-            _id: {
-                $in: req.body,
-            },
-        });
-        // res.status(200).json(a);
-        res.status(200).json('Departmet has been deleted.');
-    } catch (err) {
-        res.status(500).json(err);
-    }
+	try {
+		await Room.deleteMany({
+			_id: {
+				$in: req.body,
+			},
+		});
+		// res.status(200).json(a);
+		res.status(200).json('Departmet has been deleted.');
+	} catch (err) {
+		res.status(500).json(err);
+	}
 });
 
 export default router;
