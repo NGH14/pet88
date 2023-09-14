adffd24a82fc2b0134cb0956ebf60c43b4d50fc7
nghia <naovu@protonmail.com>
2023-09-14 21:59:41 +0700

    feat(index file): :art: listen on disconnect db

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -3,9 +3,11 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
 	try {
 		mongoose.connect(process.env.MONGO, { dbName: process.env.DB });
-
-		console.log('✅ MongoDB Connected');
 	} catch (error) {
 		console.log(error);
 	}
 };
+
+mongoose.connection.on('disconnected', () => {
+	console.log('MongoDB Disconnected');
+});
