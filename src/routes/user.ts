72dd983bed563f0e617c820e8afa3fca28a76dca
nghia <naovu@protonmail.com>
2023-09-18 22:50:12 +0700

    feat: :art: user CRUD in router

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -1,13 +1,21 @@
-
 /**
  * @swagger
  * tags:
- *   name: Users
+ *   name: users
  *   description: The Users managing API
  */
 
 import express from 'express';
-import { CreateUser, GetAllUser } from '../controller/user.ts';
+import {
+	CreateUser,
+	GetAllUser,
+	UpdateUserById,
+	DeleteUserById,
+	DeleteUsers,
+	GetUserByID,
+	CreateUsers,
+	DeleteUserByEmail,
+} from '../controller/user.ts';
 
 const router = express.Router();
 
@@ -22,8 +30,16 @@ const router = express.Router();
  *         description: Get list users success
  */
 router.get('/', GetAllUser);
+router.get('/:id', GetUserByID);
 
 router.post('/', CreateUser);
+router.post('/list', CreateUsers);
 
+router.put('/:id', UpdateUserById);
+
+router.delete('/:id', DeleteUserById);
+router.patch('/:email', DeleteUserByEmail);
+
+router.patch('/multiple-delete', DeleteUsers);
 
 export default router;
