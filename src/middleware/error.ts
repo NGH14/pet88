4233e9a047a4905f1c79c0f6a9ae56d7cd48c56a
nghia <naovu@protonmail.com>
2023-09-18 22:55:19 +0700

    feat(index file): :art: error handler

--- src/middleware/error.ts
+++ src/middleware/error.ts
@@ -1,18 +1,18 @@
 import { Request, Response, NextFunction } from 'express';
 
 const ErrorHandler = (
-	err: any,
+	error: any,
 	req: Request,
 	res: Response,
 	next: NextFunction,
 ) => {
-	const status = err.statusCode || 500; 
+	const status = error.statusCode || 500;
 	res.status(status).json({
 		success: false,
 		status,
-		message: err.message || 'Something went wrong',
-		// Just who to stack (file directory error) when development
-		stack: process.env.NODE_ENV !== 'development' ? {} : err.stack,
+		message: error.message || 'Something went wrong',
+		// Just show to stack (file directory got error) when development
+		stack: process.env.NODE_ENV !== 'development' ? {} : error.stack,
 	});
 };
 
