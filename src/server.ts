293d68572b5ce44298d0728ec1c056624f785373
nghia <naovu@protonmail.com>
2023-09-18 22:50:44 +0700

    feat(index file): :art: swagger doc and logger depend on evironment

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,18 @@
 import { connectDB } from './db/mongodb.js';
 
-import { app } from './app.ts';
+import app from './app.ts';
 import logger from '../src/utils/logger.ts';
 import swaggerDocs from '../src/utils/swagger.ts';
 const PORT: number = Number(process.env.LOCAL_PORT || 5001);
 
 app.listen(PORT, () => {
-
 	connectDB();
-	if (process.env.NODE_ENV == "development") swaggerDocs(app, PORT);
-
-	logger.log({
-		level: 'info',
-		message: 'Server listening on',
-    data: `http://localhost:${PORT}`
-	});
+	if (process.env.NODE_ENV == 'development') swaggerDocs(app, PORT);
+	if (process.env.NODE_ENV !== 'test') {
+		logger.log({
+			level: 'info',
+			message: 'Server listening on',
+			data: `http://localhost:${PORT}`,
+		});
+	}
 });
