18e5b046e7fdec18f1a37046dd5b318ea81cb422
nghia <naovu@protonmail.com>
2023-09-21 23:41:13 +0700

    fix: :bug: remove success middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import express from 'express';
@@ -6,13 +5,12 @@ import multer from 'multer';
 import sharp from 'sharp';
 import cloudinary from './config/cloudinary.js';
 import './config/firebase.js';
-import ErrorHandler from "./middleware/error.ts"
+import ErrorHandler from './middleware/error.ts';
 import rootRouter from './routes/index.ts';
-
 
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(express.static('public'));
 app.use(express.json({ limit: '10mb' }));
 app.use(
@@ -36,32 +34,9 @@ app.use(
 	}),
 );
 
-
 // app.use(middleware.decodeToken);
 
-
-// let upload = multer();
-
-// app.post('/test-image', upload.single('image'), async (req, res) => {
-// 	const data = await sharp(req?.file?.buffer).webp().toBuffer();
-// 	const folder = await cloudinary.api.create_folder('test3');
-// 	cloudinary.uploader
-// 		.upload_stream(
-// 			{ public_id: 'test_upload2', folder: folder?.name },
-// 			(error) => {
-// 				if (error) {
-// 					res.status(500).send(error);
-// 				} else {
-// 					res.json('File uploaded successfully:');
-// 				}
-// 			},
-// 		)
-// 		.end(data);
-// });
-
 app.use('/api/v1/', rootRouter);
-app.use(ErrorHandler)
-
+app.use(ErrorHandler);
 
 export default app;
-
