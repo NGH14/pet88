72df35a4dcf56754eef3271dfb9cbd9cc80129ab
nghia <naovu@protonmail.com>
2023-10-05 00:36:49 +0700

    feat(index file): :art: refactor: sendmail test

--- v2/tests/sendmail.test.ts
+++ v2/tests/sendmail.test.ts
@@ -1,49 +1,36 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import app from '../src/app.ts';
-
+import sendMail from '../src/utils/sendmail.ts';
 const should = chai.should();
 const expect = chai.expect;
 import { randCity, randStreetAddress } from '@ngneat/falso';
+import { SENDER } from '../src/constants/email.ts';
 
 chai.use(chaiHttp);
 
-
 describe('Send Mail API', () => {
-const baseUrl = '/api/v2/' + "email";
+	const baseUrl = '/api/v2/' + 'email';
 
-  it('should POST to send mail', (done) => {
-    const mail = {    
-      "data": {
-       "subject": "Pet88: Test Send Mail",
-        "to": "servicepet88@gmail.com"
-      },
-      "template": "<p> test </p>"
-  };
-    chai
-      .request(app)
-      .post(baseUrl)
-      .set('content-type', 'application/json')
-      .send(JSON.stringify(mail))
-      .end((_err, res) => {
-        res.should.have.status(200);
-        res.body.message.should.be.contain("sent");
-      });
-    done();
-  });
-  it('should not POST to send the missing data mail', (done) => {
-    const mail = {    
-      "data": {},
-      "template": "<p> test </p>"
-  };
-    chai
-      .request(app)
-      .post(baseUrl)
-      .set('content-type', 'application/json')
-      .send(JSON.stringify(mail))
-      .end((_err, res) => {
-        res.should.have.status(500);
-      });
-    done();
-  });
+	it('should POST to send mail', async () => {
+		const dataSend = {
+			to: SENDER.email,
+			subject: 'Test email',
+			text: 'This is a test email.',
+		};
+
+		const template = `
+		<html>
+			<head>
+				<title>Test email</title>
+			</head>
+			<body>
+				<h1>This is a test email.</h1>
+			</body>
+		</html>`;
+
+		const info = await sendMail(dataSend, template);
+
+		info.response.should.contain(250);
+	});
 });
