b1a42f7564f384a5b709b8cdefe8a83b8e0a3232
nghia <naovu@protonmail.com>
2025-02-05 02:03:25 +0700

    refactor: remove deprecated files and constants

--- v2/src/routes/auth.ts
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { registerUser, login } from '../controller/auth.ts';
-const router = express.Router();
-
-router.post("/register", registerUser);
-router.post("/login", login)
-export default router;
