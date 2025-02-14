81a022bbe2b7dcc5b8b4e959c793c1d93c78ced3
nghia <naovu@protonmail.com>
2025-02-14 13:23:08 +0700

    feat: add Storybook stories for Button, Calendar, Card, Header, Message, Notification, Page, Steps, and UploadFile components

---
+++ frontend/src/stories/Header.stories.jsx
@@ -0,0 +1,23 @@
+
+import { Header } from '../components/Header/';
+
+export default {
+  title: 'COMPONENTS/Header',
+  component: Header,
+  parameters: {
+    // More on Story layout: https://storybook.js.org/docs/react/configure/story-layout
+    layout: 'fullscreen',
+  },
+};
+
+const Template = (args) => <Header {...args} />;
+
+export const LoggedIn = Template.bind({});
+LoggedIn.args = {
+  user: {
+    name: 'Nghia Vu',
+  },
+};
+
+export const LoggedOut = Template.bind({});
+LoggedOut.args = {};
