59610786ef733fcd1c245259429c7c0944d72dad
nghia <naovu@protonmail.com>
2025-02-15 00:26:36 +0700

    refactor: comment out LanguageContext implementation for future localization updates

--- frontend/src/context/LanguageContext.jsx
+++ frontend/src/context/LanguageContext.jsx
@@ -1,24 +1,24 @@
-import React, { createContext, useContext } from 'react';
+// import React, { createContext, useContext } from 'react';
 
-const LanguageContext = createContext();
+// const LanguageContext = createContext();
 
-export const LanguageContextProvider = ({ children }) => {
-	const [lang, setLang] = React.useState(
-		localStorage.getItem('lang') || "en_US",
-	);
+// // export const LanguageContextProvider = ({ children }) => {
+// // 	const [i18n.language, setLang] = React.useState(
+// // 		localStorage.getItem('i18n.language') || "en_US",
+// // 	);
 
-	const SetLanguage = (lg) => {
-		localStorage.setItem('lang', lg);
-		setLang(lg);
-	};
+// // 	const SetLanguage = (lg) => {
+// // 		localStorage.setItem('i18n.language', lg);
+// // 		setLang(lg);
+// // 	};
 
-	return (
-		<LanguageContext.Provider value={{ lang, SetLanguage }}>
-			{children}
-		</LanguageContext.Provider>
-	);
-};
+// // 	return (
+// // 		<LanguageContext.Provider value={{ i18n.language, SetLanguage }}>
+// // 			{children}
+// // 		</LanguageContext.Provider>
+// // 	);
+// // };
 
-export const UserLanguage = () => {
-	return useContext(LanguageContext);
-};
+// export const UserLanguage = () => {
+// 	return useContext(LanguageContext);
+// };
