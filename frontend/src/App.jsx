0cda0dba3844450fac68c5f90e492ccfba9744b1
nghia <naovu@protonmail.com>
2025-02-16 02:02:35 +0700

    feat: update i18next extract configuration and refactor App component structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,10 @@
 import * as React from 'react';
+import { ThemeProvider } from 'styled-components';
 import { I18nextProvider } from 'react-i18next';
 import { BrowserRouter, Routes } from 'react-router';
-
 import { AuthContextProvider } from 'context/AuthContext';
 import { ToastContainer } from 'react-toastify';
 import { SearchContextProvider } from 'context/SearchContext';
-import { withNamespaces } from 'react-i18next';
 import { routes } from './routes/';
 import ListRoutes from 'routes/ListRoutes';
 import i18n from 'i18next';
@@ -17,8 +16,9 @@ import './App.css';
 function App() {
 	return (
 		<AuthContextProvider>
-				<I18nextProvider i18n={i18n}>
-					<SearchContextProvider>
+			<I18nextProvider i18n={i18n}>
+				<SearchContextProvider>
+					<ThemeProvider theme={styledTheme}>
 						<BrowserRouter>
 							<ToastContainer
 								position='top-right'
@@ -33,8 +33,9 @@ function App() {
 							/>
 							<Routes>{ListRoutes(routes)}</Routes>
 						</BrowserRouter>
-					</SearchContextProvider>
-				</I18nextProvider>
+					</ThemeProvider>
+				</SearchContextProvider>
+			</I18nextProvider>
 		</AuthContextProvider>
 	);
 }
--- frontend/src/i18n/constants.js
+++ frontend/src/i18n/constants.js
@@ -1,28 +1,26 @@
-import vi_VN from "./locales/vi_VN/translation.json"
-import en_US from "./locales/en_US/translation.json"
-
+import vi_VN from './locales/vi_VN/translation.json';
+import en_US from './locales/en_US/translation.json';
 
 const resources = {
-  vi_VN: {
-    translation: vi_VN,
-  },
-  en_US: {
-    translation: en_US,
-  },
+	vi_VN: {
+		translation: vi_VN,
+	},
+	en_US: {
+		translation: en_US,
+	},
 };
 
-
 const i18nConfig = {
-  resources,
-  fallbackLng: 'en_US',
-  interpolation: {
-    escapeValue: false,
-  },
-  detection: {
-    order: ['localStorage'],
-    lookupLocalStorage: 'lang',
-    checkWhitelist: true,
-  },
-}
+	resources,
+	fallbackLng: 'en_US',
+	interpolation: {
+		escapeValue: false,
+	},
+	detection: {
+		order: ['localStorage'],
+		lookupLocalStorage: 'lang',
+		checkWhitelist: true,
+	},
+};
 
 export default i18nConfig;
