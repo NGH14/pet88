5959d21a738f7b95f43b8af689539a959fc82bf0
nghia <naovu@protonmail.com>
2025-02-17 13:16:59 +0700

    feat: add stories for Notification component to enhance documentation

---
+++ frontend/src/components/Notification/notification.stories.js
@@ -0,0 +1,35 @@
+import { Notification } from 'components/Notification/';
+export default {
+  title: 'COMPONENTS/Notification',
+  component: Notification,
+  parameters: {
+    layout: 'centered',
+  },
+  argTypes: { onClick: { action: 'closed' } },
+};
+const Template = (args) => <Notification {...args} />;
+export const defaultToast = Template.bind({});
+export const warningToast = Template.bind({});
+warningToast.args = {
+  types: 'warning',
+};
+export const informationToast = Template.bind({});
+informationToast.args = {
+  types: 'info',
+};
+export const errorToast = Template.bind({});
+errorToast.args = {
+  types: 'error',
+};
+export const successToast = Template.bind({});
+successToast.args = {
+  types: 'success',
+};
+export const darkMode = Template.bind({});
+darkMode.args = {
+  theme: 'dark',
+};
+export const coloredMode = Template.bind({});
+coloredMode.args = {
+  theme: 'colored',
+};
