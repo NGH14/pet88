2ac5efe23bfb2aa63239f194897beab94aec651f
nghia <naovu@protonmail.com>
2025-02-17 20:56:55 +0700

    refactor: create NavLink component with styled components and update styles

---
+++ frontend/src/components/NavLink/NavLink.jsx
@@ -0,0 +1,3 @@
+import {StyledNavLink, NavbarLinkContainer} from "./NavLink.style.js"
+
+export { StyledNavLink, NavbarLinkContainer };
--- frontend/src/components/NavLink/NavLink.style.js
+++ frontend/src/components/NavLink/NavLink.style.js
@@ -4,23 +4,18 @@ import styled from 'styled-components';
 export const NavbarContainer = styled.nav`
 	width: 100%;
 	height: 50px;
-	background-color: purple;
 	display: flex;
 	flex-direction: column;
 `;
 
-export const NavbarLinkContainer = styled.section`
-	display: flex;
-`;
 export const StyledNavLink = styled(NavLink)`
 	color: #171111;
 	text-decoration: none;
 	margin: 10px;
+	font-size: 1.25em;
 	&:hover,
-	&:focus {
-		color: blue;
-	}
+	&:focus,
 	&:active {
-		color: red;
+		color: #f76a1a;
 	}
 `;
