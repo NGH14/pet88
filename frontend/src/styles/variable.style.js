f07b7852dab51f6263071d51c06fb9794e7b6050
nghia <naovu@protonmail.com>
2025-02-18 13:19:17 +0700

    feat: add pure black and pure white color variables to styles

--- frontend/src/styles/variable.style.js
+++ frontend/src/styles/variable.style.js
@@ -2,6 +2,8 @@ import { css } from 'styled-components';
 
 export const color = css`
 	--primary-color: #f76a1a;
+	--pure-black: #171111;
+	--pure-white: #f7f6f1;
 `;
 
 export const variable = css`
