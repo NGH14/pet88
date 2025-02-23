c119d1e15f605c53e9dd2a2158ee15a93cadb187
nghia <naovu@protonmail.com>
2025-02-24 00:39:18 +0700

    fix: update working hours format in contact constants for consistency

--- frontend/src/constants/contact.mjs
+++ frontend/src/constants/contact.mjs
@@ -2,28 +2,26 @@ export default {
 	workingTime: {
 		title: 'working time',
 		schedule: [
-
-				{
-					"day": "Monday",
-					"time": "9:00 AM–5:00 PM"
-				},
-				{
-					"day": "Tuesday",
-					"time": "9:00 AM–6:00 PM"
-				},
-				{
-					"day": "Thursday",
-					"time": "9:00 AM–4:00 PM"
-				},
-				{
-					"day": "Friday",
-					"time": "9:00 AM–5:00 PM"
-				},
-				{
-					"day": "Saturday",
-					"time": "9:00 AM–3:00 PM"
-				}
-
+			{
+				day: 'Monday',
+				time: '9:00 AM–5:00 PM',
+			},
+			{
+				day: 'Tuesday',
+				time: '9:00 AM–6:00 PM',
+			},
+			{
+				day: 'Thursday',
+				time: '9:00 AM–4:00 PM',
+			},
+			{
+				day: 'Friday',
+				time: '9:00 AM–5:00 PM',
+			},
+			{
+				day: 'Saturday',
+				time: '9:00 AM–3:00 PM',
+			},
 		],
 	},
 	email: {
