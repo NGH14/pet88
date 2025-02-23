f5da245dda6f1a41c7dee123d2423256f5c1f1da
nghia <naovu@protonmail.com>
2025-02-23 18:49:13 +0700

    chore: relocation of i18n folder

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 
 import App from './App';
-import './i18n';
+import 'utils/i18n/index.js';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<App />);
