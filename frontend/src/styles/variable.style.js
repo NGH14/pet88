5a81ea75c539bb62ade6425e787dcfc473861ff2
nghia <naovu@protonmail.com>
2025-02-24 00:20:11 +0700

    refactor: update color variables in variable.style.js for improved design consistency

--- frontend/src/styles/variable.style.js
+++ frontend/src/styles/variable.style.js
@@ -4,7 +4,14 @@ export const color = styled.css`
 	--primary-color: #f76a1a;
 	--pure-black: #171111;
 	--dark-gray: #333333;
-	--gray-300: #f3f3f3;
+	--gray-100: #f5f5f5;
+	--gray-300: #d3d3d3;
+	--gray-500: #a9a9a9;
+	--gray-700: #7f7f7f;
+	--green-100: #88E788;
+	--green-300: #00B300;
+	--green-500: #008000;
+	--green-700: #004C00;
 	--white-300: #fafafa;
 	--pure-white: #ffffff;
 	--light-white: #f7f6f1;
