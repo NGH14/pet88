f87435abd8c09e47575e21dc9b0189f061d0cb35
nghia <naovu@protonmail.com>
2025-02-24 00:40:40 +0700

    refactor: update color variables in variable.style.js for hex code

--- frontend/src/styles/variable.style.js
+++ frontend/src/styles/variable.style.js
@@ -1,21 +1,21 @@
 import * as styled from 'styled-components';
 
 export const color = styled.css`
-	--primary-color: #f76a1a;
+	--primary-color: #F76A1A;
 	--pure-black: #171111;
 	--dark-gray: #333333;
-	--gray-100: #f5f5f5;
-	--gray-300: #d3d3d3;
-	--gray-500: #a9a9a9;
-	--gray-700: #7f7f7f;
-	--green-100: #88e788;
-	--green-300: #00b300;
+	--gray-100: #F5F5F5;
+	--gray-300: #D3D3D3;
+	--gray-500: #A9A9A9;
+	--gray-700: #7F7F7F;
+	--green-100: #88E788;
+	--green-300: #00B300;
 	--green-500: #008000;
-	--green-700: #004c00;
-	--white-300: #fafafa;
-	--pure-white: #ffffff;
-	--light-white: #f7f6f1;
-	--color-white: #e5e5e5;
+	--green-700: #004C00;
+	--white-300: #FAFAFA;
+	--pure-white: #FFFFFF;
+	--light-white: #F7F6F1;
+	--color-white: #E5E5E5;
 `;
 
 export const variable = styled.css`
