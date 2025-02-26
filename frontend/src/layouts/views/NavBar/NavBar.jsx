ee915792d5fa7405ce3d706157426535618d132d
nghia <naovu@protonmail.com>
2025-02-27 02:41:30 +0700

    refactor: clean up NavBar component by removing unnecessary whitespace and improving CSS formatting

--- frontend/src/layouts/views/NavBar/NavBar.jsx
+++ frontend/src/layouts/views/NavBar/NavBar.jsx
@@ -3,8 +3,6 @@ import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { NavLink, useLocation, useNavigate } from 'react-router';
 
-
-
 import { CloseOutlined, LogoutOutlined, MenuOutlined } from '@ant-design/icons';
 import { Button, Drawer } from 'antd';
 import AuthButton from 'components/AuthButton/AuthButton.jsx';
@@ -14,13 +12,7 @@ import { StyledNavLink } from 'components/NavLink/index.jsx';
 import { UserAuth } from 'context/AuthContext';
 import useScrollPosition from 'hooks/useScrollPosition';
 
-
-
 import { NavBarList, StyledNavBar } from './Navbar.style.js';
-
-
-
-
 
 const pages = [
 	{ title: 'About', url: '/#about' },
