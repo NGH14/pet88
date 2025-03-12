edf808d924ea35721b126c6ebd6c1095c75da04a
nghia <naovu@protonmail.com>
2025-03-12 18:06:02 +0700

    refactor: move Logo component styling to separate file; export DefaultLogo for improved organization

--- frontend/src/components/Logo/Logo.jsx
+++ frontend/src/components/Logo/Logo.jsx
@@ -1,10 +1,1 @@
-import PrimaryWebP from 'assets/images/logo/icon-logo-primary.webp';
-import { styled } from 'styled-components';
-
-export const Logo = styled.img.attrs(props => ({
-	src: props.Img || PrimaryWebP,
-	alt: 'Pet88 Logo',
-}))`
-	${props => (props.maxHeight ? `max-height: ${props.maxHeight}` : 'max-height: 32px')};
-	${props => props.dark && `filter: brightness(0%)`};
-`;
+export { DefaultLogo as Logo } from './Logo.style.mjs';
---
+++ frontend/src/components/Logo/Logo.style.mjs
@@ -0,0 +1,10 @@
+import PrimaryWebP from 'assets/images/logo/icon-logo-primary.webp';
+import { styled } from 'styled-components';
+
+export const DefaultLogo = styled.img.attrs(props => ({
+  src: props.Img || PrimaryWebP,
+  alt: 'Pet88 Logo',
+}))`
+	${props => (props.maxHeight ? `max-height: ${props.maxHeight}` : 'max-height: 32px')};
+	${props => props.dark && `filter: brightness(0%)`};
+`;
