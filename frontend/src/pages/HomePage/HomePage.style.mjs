4e42a3cd5bd952c4f344b97788f639c0dff8b460
nghia <naovu@protonmail.com>
2025-03-14 21:31:24 +0700

    refactor: rename LightBackground to ColoredBackGround and update background in styled component

--- frontend/src/pages/HomePage/HomePage.style.mjs
+++ frontend/src/pages/HomePage/HomePage.style.mjs
@@ -1,7 +1,7 @@
 import { styled } from 'styled-components';
 
-export const LightBackground = styled.section`
-	background-color: color-mix(in hsl, var(--primary-200) 20%, transparent);
+export const ColoredBackGround = styled.section`
+	background-color: ${props => props.$bgColor ?  : 'color-mix(in hsl, var(--primary-200) 20%,transparent)'};
 	padding: 2em 1em;
 	border-radius: 45px;
 	margin: 1.5em calc(-1 * var(--global-margin)) 0;
